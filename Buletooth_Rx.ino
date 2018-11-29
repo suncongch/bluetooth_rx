c828365aa9d0b7cdb563dcf1d4f37bc7c000d090
suncongch <32638064+suncongch@users.noreply.github.com>
2018-11-29 19:33:39 +0800

    Add files via upload

---
+++ Buletooth_Rx.ino
@@ -0,0 +1,13 @@
+String data = "";
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    data=Serial.readString();
+    Serial.print(data.toInt());
+    digitalWrite(2,data.toInt());
+  }
+}
